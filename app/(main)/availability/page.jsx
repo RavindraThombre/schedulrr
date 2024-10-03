0ba08780c2df832a76bd1c5a145bf49c4a27f10f
Ravindra Thombre <ravi@gmail.com>
2024-10-03 20:11:18 +0530

    Depolyment

---
+++ app/(main)/availability/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { getUserAvailability } from "@/actions/availability";
+import AvailabilityForm from "./_components/availability-form";
+import { defaultAvailability } from "./data";
+
+export default async function AvailabilityPage() {
+  const availability = await getUserAvailability();
+
+  return <AvailabilityForm initialData={availability || defaultAvailability} />;
+}
---
+++ components/create-event.jsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { useRouter, useSearchParams } from "next/navigation";
+import EventForm from "./event-form";
+
+export default function CreateEventDrawer() {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const create = searchParams.get("create");
+    if (create === "true") {
+      setIsOpen(true);
+    }
+  }, [searchParams]);
+
+  // State can be exposed to our app in case we want to manually open the drawer 👇
+  // useEffect(() => {
+  //   window.openCreateEventDrawer = () => setIsOpen(true);
+
+  //   return () => {
+  //     delete window.openCreateEventDrawer;
+  //   };
+  // }, []);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    if (searchParams.get("create") === "true") {
+      router.replace(window?.location.pathname);
+    }
+  };
+
+  return (
+    <Drawer open={isOpen} onClose={handleClose}>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Create New Events</DrawerTitle>
+        </DrawerHeader>
+
+        <EventForm
+          onSubmitForm={() => {
+            handleClose();
+          }}
+        />
+        <DrawerFooter className="px-6">
+          <Button>Submit</Button>
+          <DrawerClose asChild>
+            <Button variant="outline" onClick={handleClose}>
+              Cancel
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
